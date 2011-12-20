f29da0ef4b575fe7f626803fbb08e8eacf3432dd
ikarosdev <ikarosdev@gmail.com>
2011-12-20 12:21:25 -0600

    display: add conditional around libgenlock for devices who don't have genlock in kernel space yet.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
 #Enables the listed display HAL modules
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	display-hals := libhwcomposer liboverlay libgralloc libcopybit libgenlock
+	display-hals := libhwcomposer liboverlay libgralloc libcopybit
+ifeq ($(BOARD_USES_GENLOCK),true)
+	display-hals += libgenlock
+endif
 	display-hals += libqcomui
 	include $(call all-named-subdir-makefiles,$(display-hals))
 endif
