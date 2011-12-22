4f80294ab7a28bd309120721fa717ea6f512e65f
Isaac <ikarosdev@gmail.com>
2011-12-22 12:12:05 -0600

    display: fix makefile

--- Android.mk
+++ Android.mk
@@ -1,10 +1,7 @@
 #Enables the listed display HAL modules
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	display-hals := libhwcomposer liboverlay libgralloc libcopybit
-ifeq ($(BOARD_USES_GENLOCK),true)
-	display-hals += libgenlock
-endif
+	display-hals := libhwcomposer liboverlay libgralloc libcopybit libgenlock libtilerenderer
 	display-hals += libqcomui
 	include $(call all-named-subdir-makefiles,$(display-hals))
 endif
